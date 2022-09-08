1259e79c84c95d97d751d7dfe7b106d017b81da6
dsilv <dsilv@Osiris.dhcp.wustl.edu>
2022-09-08 12:42:52 -0500

    Studio 1

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,16 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Is the year a leap or not?");
+		int n1 = in.nextInt();
+		boolean leapyear = n1%4 == 0 && n1%100 > 0 || n1%400 ==0;
+		System.out.println("Is "+n1+" a leap year? "+leapyear);
+	}
+
+}
